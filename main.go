8cd7247c5240d4639fda31ce184e2e95b33e33e3
Hidenari-Yuda <106872451+hidenari-yuda@users.noreply.github.com>
2023-02-11 18:16:49 +0900

    fix rand.Seed

--- main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "github.com/TheAlgorithms/Go/strings/genetic"
-
-func main() {
-	rune := []rune("Hello, World!")
-	res, _ := genetic.GeneticString("Hello, World!", rune, &genetic.Conf{})
-	println(res)
-}
